94245f46c06b02b7327c48ee06397b312e41e091
Andrey <andrey.gonzalez.villegas@est.una.ac.cr>
2026-06-17 16:26:11 -0600

    configuracion de SMTP con Mailtrap y pruebas de envio de correos

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<GlobalContex>(options =>
 builder.Services.AddScoped<IFormSacraService, FormSacraService>();
 builder.Services.AddScoped<IInscripcionCatequesisService, InscripcionCatequesisService>();
 builder.Services.AddScoped<IDonacionService, DonacionService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 builder.Services.AddScoped<IBautismoService, BautismoService>();
 builder.Services.AddScoped<IComunionService, ComunionService>();
---
+++ Services/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace SanblasBackend.Services;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string toEmail, string subject, string body);
+}
