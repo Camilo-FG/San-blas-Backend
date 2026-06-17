deb809b46bdfef1cc3fad4102b6262eb6ae15586
Andrey <andrey.gonzalez.villegas@est.una.ac.cr>
2026-06-17 01:25:54 -0600

    implementar DonacionService, IDonacionService y DTOs

---
+++ Services/IDonacionService.cs
@@ -0,0 +1,12 @@
+﻿using SanblasBackend.DTOs;
+using SanblasBackend.Models;
+
+namespace SanblasBackend.Services;
+
+public interface IDonacionService
+{
+    Task<IEnumerable<Donacion>> GetAllDonaciones();
+    Task<Donacion?> GetDonacionById(int id);
+    Task<Donacion> CreateDonacion(DonacionCreateDto dto);
+    Task<Donacion?> UpdateEstado(int id, string nuevoEstado);
+}
