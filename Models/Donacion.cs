af59b7926e3e6be0e1266b6bcda9c5da32954174
Andrey <andrey.gonzalez.villegas@est.una.ac.cr>
2026-06-16 23:03:14 -0600

    agregar modelo de entidad Donacion

---
+++ Models/Donacion.cs
@@ -0,0 +1,20 @@
+﻿namespace SanblasBackend.Models;
+
+public class Donacion
+{
+    public int Id { get; set; }
+
+    public DateTime Fecha { get; set; }
+
+    public bool Anonimo { get; set; }
+
+    public string Nombre { get; set; } = string.Empty;
+
+    public string Correo { get; set; } = string.Empty;
+
+    public string? Telefono { get; set; }
+
+    public string Detalle { get; set; } = string.Empty;
+
+    public string Estado { get; set; } = "Pendiente";
+}
