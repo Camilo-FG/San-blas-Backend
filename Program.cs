ae859b2c4f0e621135e7e5912599f98bd576f297
Andrey <andrey.gonzalez.villegas@est.una.ac.cr>
2026-06-17 18:44:23 -0600

    habilitar CORS global y configurar envio de correos SMTP real

--- Program.cs
+++ Program.cs
@@ -31,6 +31,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin() 
+              .AllowAnyMethod() 
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -40,6 +50,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
 
 app.UseAuthorization();
 
