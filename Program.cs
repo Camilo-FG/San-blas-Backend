5ec72dda519eb09c42bd8170ee0332fcabdab6a3
Gvrh05 <delizondogutierrez@gmail.com>
2026-06-17 18:29:31 -0600

    correcciones importantes

--- Program.cs
+++ Program.cs
@@ -30,6 +30,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        policy =>
+        {
+            policy.WithOrigins(
+                    "http://localhost:5173"
+                )
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -37,6 +52,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors("AllowSpecificOrigins");
 
 app.UseHttpsRedirection();
 
